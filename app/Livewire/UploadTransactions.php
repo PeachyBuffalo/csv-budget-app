5bc3867271066de9f97165be73d65dbb62e23ab4
Macall Smith <89888292+PeachyBuffalo@users.noreply.github.com>
2024-07-02 10:30:33 -0400

    Initial commit

---
+++ app/Livewire/UploadTransactions.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class UploadTransactions extends Component
+{
+    public function render()
+    {
+        return view('livewire.upload-transactions');
+    }
+}
